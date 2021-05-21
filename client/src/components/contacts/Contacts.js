4605c94885fab07f03ad3052ff910460bd962c7f
= <majolopez1991@gmail.com>
2021-05-21 09:40:04 -0500

    add animation for contacts list

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
-import React, {Fragment, useContext} from 'react';
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition,TransitionGroup } from 'react-transition-group';
 import ContactContext from '../../context/contact/ContactContext' ;
 import ContactItem from './ContactItem';
 
@@ -14,7 +15,17 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {filtered !== null ? filtered.map ( contact => (<ContactItem key={contact.id} contact={contact}/>)) : contacts.map ( contact => (<ContactItem key={contact.id} contact={contact}/>))}
+      <TransitionGroup>
+          {filtered !== null ? filtered.map ( contact => (
+          <CSSTransition key={contact.id} timeout={1000} classNames="item">
+            <ContactItem  contact={contact}/>
+          </CSSTransition>
+          )) : contacts.map ( contact => (
+          <CSSTransition key={contact.id} timeout={1000} classNames="item">
+            <ContactItem contact={contact}/>
+          </CSSTransition>
+          ))}
+      </TransitionGroup>
     </Fragment>
   )
 };
