9ae296cade33ca8146315a5ab6cf19de3bb7b415
= <majolopez1991@gmail.com>
2021-05-24 18:25:45 -0500

    fix server file

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use('/api/auth', require('./routes/auth'));
 //Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
   //Set static folder
-  app.use(express.static('clinet/build'));
+  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => res.sendFile());
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'client', 'build')));
 }
 
 const PORT = process.env.PORT || 5000;
