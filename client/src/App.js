c2f188bfe4a96397c75c8ff9b520b45b8df58a4e
= <majolopez1991@gmail.com>
2021-05-24 16:13:55 -0500

    create private routing component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import PrivateRoute from './components/routing/PrivateRoute';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
@@ -28,7 +29,7 @@ const App = () => {
               <div className="container">
                 <Alerts/>
                 <Switch>
-                  <Route exact path="/" component={Home}/>
+                  <PrivateRoute exact path="/" component={Home}/>
                   <Route exact path="/about" component={About}/>
                   <Route exact path="/register" component={Register}/>
                   <Route exact path="/login" component={Login}/>
